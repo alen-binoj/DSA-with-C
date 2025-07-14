39d776fd67d79434b41f9a19c3869f41967ff442
alen-binoj <alenbinoj96@gmail.com>
2025-07-14 19:28:49 +0530

    error fixed in program3.c

--- DSA_lab/program3.c
+++ DSA_lab/program3.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
-void intersection(int arr1[],int arr2[],int arr[],int n1,int n2){
-    int n3=0;
-    for(int i=0;i<n1;i++){
-        for(int j=0;j<n2;j++){
-            if(arr1[i]==arr2[j]){
-                for(int k=0;k<n3;k++){
-                    if(arr[k]==arr1[i]){
-                        break; // if already present, skip
-                    }
-                    else{
-                        arr[n3]=arr1[i]; // add to intersection
-                        n3++;
-                        break; // exit inner loop after adding
+
+void intersection(int arr1[], int arr2[], int arr[], int n1, int n2, int *n3) {
+    *n3 = 0;
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (arr1[i] == arr2[j]) {
+                int alreadyPresent = 0;
+                for (int k = 0; k < *n3; k++) {
+                    if (arr[k] == arr1[i]) {
+                        alreadyPresent = 1;
+                        break;
                     }
                 }
+                if (!alreadyPresent) {
+                    arr[*n3] = arr1[i];
+                    (*n3)++;
+                }
+                break; // avoid duplicates if arr1[i] matches multiple times in arr2
             }
         }
-    }   
+    }
 }
 
+int main() {
+    int arr1[] = {1, 2, 3, 4};
+    int arr2[] = {3, 4, 5};
+    int arr3[10]; // to store intersection
+    int n3;
 
-int main(){
-    int arr1[]={1,2,3,4};
-    int arr2[]={3,4,5};
-    int arr3[10]; // array to store intersection
-    intersection(arr1, arr2, arr3, 4, 3);
+    intersection(arr1, arr2, arr3, 4, 3, &n3);
+
     printf("Intersection of the two arrays: ");
-    for(int i=0;i<10;i++){
-         
+    for (int i = 0; i < n3; i++) {
         printf("%d ", arr3[i]);
     }
+    printf("\n");
 }
